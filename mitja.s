0ad75262c059c91b0d4f56b2ad5fa321854b8239
daw2-millan22 <91274985+daw2-millan22@users.noreply.github.com>
2021-10-14 16:49:14 +0200

    Add files via upload

---
+++ mitja.s
@@ -0,0 +1,98 @@
+	.file	"mitja.c"
+	.text
+	.section	.rodata
+.LC1:
+	.string	"D\303\262na la seg\303\274ent dada: "
+.LC2:
+	.string	"%f"
+.LC3:
+	.string	"Vols continuar (s/n): "
+.LC4:
+	.string	" %s"
+.LC5:
+	.string	"La mitja \303\251s: %.2f\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	pxor	%xmm0, %xmm0
+	movss	%xmm0, -20(%rbp)
+	movl	$0, -16(%rbp)
+	movb	$115, -25(%rbp)
+	jmp	.L2
+.L3:
+	leaq	.LC1(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-24(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC2(%rip), %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	movss	-24(%rbp), %xmm0
+	movss	-20(%rbp), %xmm1
+	addss	%xmm1, %xmm0
+	movss	%xmm0, -20(%rbp)
+	addl	$1, -16(%rbp)
+	leaq	.LC3(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-25(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC4(%rip), %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+.L2:
+	movzbl	-25(%rbp), %eax
+	cmpb	$115, %al
+	je	.L3
+	cvtsi2ssl	-16(%rbp), %xmm1
+	movss	-20(%rbp), %xmm0
+	divss	%xmm1, %xmm0
+	movss	%xmm0, -12(%rbp)
+	cvtss2sd	-12(%rbp), %xmm0
+	leaq	.LC5(%rip), %rdi
+	movl	$1, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	movq	-8(%rbp), %rdx
+	xorq	%fs:40, %rdx
+	je	.L5
+	call	__stack_chk_fail@PLT
+.L5:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
